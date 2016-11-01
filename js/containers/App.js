fde7125c5cfcf22fa551660393cb9ce3c4d8795a
Kyoto Kopz <kopz9999@gmail.com>
2016-11-01 16:42:33 -0700

    use Redux DevTools Extension

--- js/containers/App.js
+++ js/containers/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import configureStore from '../store/configureStore';
 import Home from '../components/Home';
-import {renderDevTools} from '../utils/devTools';
 
 const store = configureStore();
 
@@ -15,10 +14,6 @@ export default React.createClass({
         <Provider store={store}>
           <Home />
         </Provider>
-
-        {/* only renders when running in DEV mode */
-          renderDevTools(store)
-        }
       </div>
     );
   }
--- js/store/configureStore.js
+++ js/store/configureStore.js
@@ -1,6 +1,5 @@
 import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import {devTools, persistState} from 'redux-devtools';
 import * as reducers from '../reducers/index';
 
 let createStoreWithMiddleware;
@@ -9,8 +8,7 @@ let createStoreWithMiddleware;
 if (__DEV__) {
   createStoreWithMiddleware = compose(
     applyMiddleware(thunkMiddleware),
-    devTools(),
-    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )(createStore);
 } else {
   createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
